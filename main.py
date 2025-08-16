2b613b28e9493405efdd79e6c81c83bdd3b0aba3
pietvdw67 <pietvdw67@gmail.com>
2025-08-16 15:49:56 +0200

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+
+class Main:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print('running')
+
+if __name__ == '__main__':
+    Main().run()
